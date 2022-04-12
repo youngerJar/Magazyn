85eea8bf808025ae468b90a203716bce0fa171ec
Mateusz Jarliński <m.jarlinski@o2.pl>
2022-04-12 23:02:12 +0200

    Add Magazyn.py

--- Magazyn.py
+++ Magazyn.py
@@ -1,17 +1,16 @@
 
-
 Milk = {"Name":"Milk",
          "Quantity":"120",
          "Unit":"L",
-         "Unit_price":"PLN"}
+         "Unit_price":"32"}
 Mug = {"Name":"Mug",
-         "Quantity":" 30",
+         "Quantity":"23",
          "Unit":"szt",
-         "Unit_price":"PLN"}
+         "Unit_price":"22"}
 Farba = {"Name":"Farb",
        "Quantity":"3000",
        "Unit":"L",
-       "Unit_price":"PLN"}
+       "Unit_price":"21"}
 items =[Milk, Mug, Farba]
 headers = ("Name\tQuantity\tUnit\tUnit Price (PLN)")
 sold_items= []
@@ -19,12 +18,24 @@ def get_items():
     print(headers)
     for i in items:
         print(i["Name"],i["Quantity"],i["Unit"],i["Unit_price"])
+def add_item():
+    new_item = {}
+    new_item["Name"]=input("Item Name: ")
+    new_item["Quantity"]= input("Item Quantity: ")
+    new_item["Unit"]= input("Item Unit: ")
+    new_item["Unit_price"]= input("Item price: ")
+    items.append((new_item))
+
 
 word = input("What would you like to do?")
 while word != "exit":
     if word =="show":
         get_items()
         word = input("What would you like to do?")
+    if word =="add":
+        print("Adding to warehause:")
+        add_item()
+        word= input("What would you like to do?")
 else:
     print("Exiting, bye!")
 
