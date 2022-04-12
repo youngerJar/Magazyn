e872fb1fc687a73c2a522998e25d8a9d9cc49ab0
Mateusz Jarliński <m.jarlinski@o2.pl>
2022-04-12 17:21:51 +0200

    Add Magazyn.py

--- Magazyn.py
+++ Magazyn.py
@@ -2,6 +2,10 @@ Milk = {"name":"Milk",
          "quanatity":"120",
          "unit":"l",
          "unit_price":"PLN"}
+Mug = {"name":"Coffy mug",
+         "quanatity":"30",
+         "unit":"szt",
+         "unit_price":"PLN"}
 
 
 word = input("What would you like to do?")
