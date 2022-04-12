dcf6995db256f98f04c6bcfe13991fec4daa3ba1
Mateusz Jarliński <m.jarlinski@o2.pl>
2022-04-12 17:14:45 +0200

    Add Magazyn.py

---
+++ Magazyn.py
@@ -0,0 +1,10 @@
+Milk = {"name":"Milk",
+         "quanatity":"120",
+         "unit":"l",
+         "unit_price":"PLN"}
+
+
+word = input("What would you like to do?")
+if word =="exit":
+    print("Exiting...bye!")
+    exit(1)
