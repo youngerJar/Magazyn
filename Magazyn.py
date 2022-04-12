52c05cf0a773fe03ef243983e94ce4752f05c2b4
Mateusz Jarliński <m.jarlinski@o2.pl>
2022-04-12 17:29:33 +0200

    Add Magazyn.py

--- Magazyn.py
+++ Magazyn.py
@@ -1,12 +1,16 @@
 Milk = {"name":"Milk",
          "quanatity":"120",
-         "unit":"l",
+         "unit":"L",
          "unit_price":"PLN"}
-Mug = {"name":"Coffee Mug",
+Mug = {"name":"Coffy mug",
          "quanatity":"30",
          "unit":"szt",
          "unit_price":"PLN"}
-
+Farba={"name":"Farba",
+       "quanatity":"3000",
+       "unit":"L",
+       "unit_price":"PLN"}
+       
 word = input("What would you like to do?")
 if word =="exit":
     print("Exiting...bye!")
